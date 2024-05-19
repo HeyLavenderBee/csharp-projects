ec02900e8ddac3fab9256836782e9d21d8054aff
Júlia Rodrigues <heylavenderbee@gmail.com>
2024-05-19 16:44:41 -0300

    Calculate fuel price
    This is a simple program that calculates fuel price, applying a discount depending on the quantity chosen.

---
+++ prjPrecoCombustivel/prjPrecoCombustivel/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace prjPrecoCombustivel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.txt_quantidade = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.cmb_combustivel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txt_resultado.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_resultado.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txt_resultado.Location = new System.Drawing.Point(107, 230);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(121, 23);
+            this.txt_resultado.TabIndex = 0;
+            // 
+            // txt_quantidade
+            // 
+            this.txt_quantidade.Location = new System.Drawing.Point(186, 91);
+            this.txt_quantidade.Name = "txt_quantidade";
+            this.txt_quantidade.Size = new System.Drawing.Size(120, 20);
+            this.txt_quantidade.TabIndex = 1;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(117, 154);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(99, 28);
+            this.btn_calcular.TabIndex = 2;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // cmb_combustivel
+            // 
+            this.cmb_combustivel.FormattingEnabled = true;
+            this.cmb_combustivel.Items.AddRange(new object[] {
+            "Gasolina",
+            "Álcool"});
+            this.cmb_combustivel.Location = new System.Drawing.Point(25, 90);
+            this.cmb_combustivel.Name = "cmb_combustivel";
+            this.cmb_combustivel.Size = new System.Drawing.Size(121, 21);
+            this.cmb_combustivel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Escolha um combustível";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quantidade (litros)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(103, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Posto de Gasolina";
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_sair.BackgroundImage")));
+            this.btn_sair.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_sair.Location = new System.Drawing.Point(256, 230);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(75, 64);
+            this.btn_sair.TabIndex = 9;
+            this.btn_sair.Text = "Pagar e sair";
+            this.btn_sair.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(343, 306);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_combustivel);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_quantidade);
+            this.Controls.Add(this.txt_resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.TextBox txt_quantidade;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.ComboBox cmb_combustivel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_sair;
+    }
+}
+
---
+++ prjPrecoCombustivel/prjPrecoCombustivel/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjPrecoCombustivel
+{
+    public partial class Form1 : Form
+    {
+        double vQuantidade = 0, vTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb_combustivel.Focus();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            cmb_combustivel.Focus();
+            btn_sair.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pagamento efetuado!", "Pagamento", MessageBoxButtons.OK);
+            Application.Exit();
+        }
+
+        //quando a escolha for alcool, até 20 litros é desconto de 3%, acima é 5%
+        //quando for gasolina, até 20 litros é de 5%, acima é 6% (cada um)
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            vQuantidade = Int32.Parse(txt_quantidade.Text);
+            if (cmb_combustivel.SelectedIndex == 0) //gasolina
+            {
+                if (vQuantidade <= 20)
+                {
+                    vTotal = (vQuantidade * 4.29) - (vQuantidade / 20);
+                }
+                else if (vQuantidade > 20)
+                {
+                    vTotal = (vQuantidade * 4.29) - ((vQuantidade / 100)*6);
+                }
+            }
+            else if (cmb_combustivel.SelectedIndex == 1) //alcool
+            {
+                if (vQuantidade <= 20)
+                {
+                    vTotal = (vQuantidade * 3.19) - ((vQuantidade / 100)*3);
+                }
+                else if (vQuantidade > 20)
+                {
+                    vTotal = (vQuantidade * 3.19) - ((vQuantidade / 100) * 5);
+                }
+            }
+
+            txt_resultado.Text = "R$" + vTotal.ToString();
+        }
+    }
+}
